110bb0b3d0cebcec97cde261df333f3a96d32b27
mac_a2141 <victor@piousbox.com>
2025-12-13 11:58:02 -0600

    autocommit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,11 +61,12 @@ Rails.application.configure do
   # require "syslog/logger"
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
-  # logger               = ActiveSupport::Logger.new("log/#{ENV['APP_NAME']}-#{Rails.env}.log")
-  config.logger        = ActiveSupport::Logger.new(STDOUT)
-  config.log_formatter = ::Logger::Formatter.new
-  logger.formatter     = config.log_formatter
-  config.logger        = ActiveSupport::TaggedLogging.new(logger)
+  # logger                  = ActiveSupport::Logger.new("log/#{ENV['APP_NAME']}-#{Rails.env}.log")
+  config.logger           = ActiveSupport::Logger.new(STDOUT)
+  # config.logger           = ActiveSupport::TaggedLogging.new(logger)
+  config.log_formatter    = ::Logger::Formatter.new
+  config.logger.formatter = config.log_formatter
+
 end
 
 Rails.application.config.middleware.use ExceptionNotification::Rack,
